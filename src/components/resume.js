cfb8483696df33612055f0abf0e40a9550242abb
Unknown <dhetvi@ymail.com>
2020-09-26 14:42:41 -0400

    ResumePage

--- src/components/resume.js
+++ src/components/resume.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { Grid, Cell } from 'react-mdl';
+import { Grid, Cell, List, ListItem } from 'react-mdl';
 import Education from './education';
 import Experience from './experience';
+import Skill from './skills';
 import Skills from './skills';
 
 
@@ -21,35 +22,50 @@ class Resume extends Component {
 
             <h2 style={{ paddingTop: '2em' }}>Hetvi Desai</h2>
             <h4 style={{ color: 'grey' }}>Software Developer</h4>
-            <hr style={{ borderTop: '3px solid #833fb2', width: '50%' }} />
-            <p>Dummy Data</p>
-                          
-            <hr style={{ borderTop: '3px solid #833fb2', width: '50%' }} />
+            <hr style={{ borderTop: '3px solid #833fb2', width: '100%' }} />
+            <p>
+              <List>
+                <ListItem>Over a year of experience in Python Development in an Agile environment paired with client- focused
+                Infrastructure and Network Support</ListItem>
+                <ListItem>Acquired hand-on experience in software and web development using Python, Java, C/C++, and
+            JavaScript through various personal projects</ListItem>
+                <ListItem>Adequate knowledge of OOP, Data Structures, Algorithms, SQL and Networking concepts acquired
+            through projects and online training</ListItem>
+                <ListItem>Skilled in scripting, automating software workflows and deployment pipelines</ListItem>
+                <ListItem>Passionate to build and deploy efficient Web-Services on Linux Servers</ListItem>
+                <ListItem>
+                  Proficient in collaborating and communicating with various teams and international clients</ListItem>
+                <ListItem>Developed analytical and problem-solving skills by resolving critical technical issues</ListItem>
+              </List>
+            </p>
+
+            <hr style={{ borderTop: '3px solid #833fb2', width: '100%' }} />
             <h5>Address</h5>
-            <p>1 Hacker Way Menlo Park, 94025</p>
-            <h5>Phone</h5>
-            <p>(123) 456-7890</p>
+            <p>Ontario, Canada </p>
+            
             <h5>Email</h5>
-            <p>someone@example.com</p>
+            <p>dhetvi@ymail.com</p>
+           
             <h5>Web</h5>
             <p>mywebsite.com</p>
-            <hr style={{ borderTop: '3px solid #833fb2', width: '50%' }} />
+            
+            <hr style={{ borderTop: '3px solid #833fb2', width: '100%' }} />
           </Cell>
-          
+
           <Cell className="resume-right-col" col={8}>
             <h3>Education</h3>
 
 
             <Education
-              startYear={2019}
-              endYear={2020}
+              startYear={'Sept 2019'}
+              endYear={'Dec 2020'}
               schoolName="University of Windsor"
               schoolDescription="Master of Applied Computing"
             />
 
             <Education
-              startYear={2014}
-              endYear={2018}
+              startYear={'Aug 2014'}
+              endYear={'May 2018'}
               schoolName="Dharmsinh Desai University"
               schoolDescription="Bachelor of Technology in Information Technology"
             />
@@ -62,33 +78,34 @@ class Resume extends Component {
               endYear={'Ongoing'}
               jobName="Software Developer Intern"
               companyName="Coveo"
-              jobDescription="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
-            />
+              jobDescription=""
+                          />
 
             <Experience
-              startYear={2012}
-              endYear={2016}
-              jobName="Second Job"
-              jobDescription="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
+              startYear={'July 2018'}
+              endYear={'July 2019'}
+              jobName="System Engineer"
+              companyName="Infosys Limited"
+              jobDescription=""
+            />
+            <Experience
+              startYear={'Feb 2018'}
+              endYear={'May 2018'}
+              jobName="System Engineer Intern"
+              companyName="Infosys Limited"
+              
+              jobDescription=""
             />
             <hr style={{ borderTop: '3px solid #e22947' }} />
-            {/* <h2>Skills</h2>
-            <Skills
-              skill="javascript"
-              progress={100}
-            />
-            <Skills
-              skill="HTML/CSS"
-              progress={80}
-            />
-            <Skills
-              skill="NodeJS"
-              progress={50}
-            />
-            <Skills
-              skill="React"
-              progress={25}
-            /> */}
+            
+            <h3>Skills</h3>
+            
+            <Skill main="Programming Languages" skill="Python, Java, C#, C/C++, JavaScript, Shell Script/PowerShell"/>
+            <Skill main="Frameworks/Libraries" skill=" Django, NodeJS, TypeScript, AngularJS, ReactJS, Pandas, Junit, Hibernate"/>
+            <Skill main="Database" skill="MySQL, Oracle DB, MongoDB, MS Access"/>
+            <Skill main="OS Familiarity" skill="Linux, Windows, Android, iOS"/>
+            <Skill main="Cloud Services" skill="Azure SQL, Compute, AWS Lambda, Azure Devops, Firebase"/>
+            <Skill main= "Miscellaneous" skill=" Git, MS Office Suite, ServiceNow, BigPanda, Wireshark, CI/CD, Docker, Ansible"/>
 
 
           </Cell>
